81b075b756b1de4f61b81967dc5336beaab40cde
demo0302 <nandinisingh1622@gmail.com>
2025-03-22 02:18:25 +0530

    � Fixed frontend serving issue

--- space-travel/backend/index.js
+++ space-travel/backend/index.js
@@ -29,6 +29,14 @@ const connect = async () => {
     console.log("❌ MongoDB connection failed", error);
   }
 };
+import path from "path";
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 app.use(express.json());
 app.use(express.static("public"));
